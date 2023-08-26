73aa45a54f3458b7016565a9ba355b85341ad7db
Hian A. Damaceno <132616428+55Hian@users.noreply.github.com>
2023-08-26 15:24:49 +0000

    codigos aula 2

--- aula2.py
+++ aula2.py
@@ -1,1 +1,26 @@
+import time
+import matplotlib.pyplot as plt
 
+x = []
+y = []
+problem_size = 1000000
+print(f"{'problemSize':>12}{'Seconds':>16}")
+for count in range(5):
+    start = time.time()
+    work = 1
+    for _ in range(problem_size):
+        work += 1
+        work -= 1
+    elapsed = time.time() - start
+    print(f"{problem_size:>12}{elapsed:>16.3f}")
+    x.append(problem_size)
+    y.append(elapsed)
+    problem_size *= 2
+
+plt.plot(x, y)
+plt.xlabel("Problem Size")
+plt.ylabel("Time (Seconds)")
+plt.title("Execution Time vs Problem Size")
+plt.xscale("log")
+plt.yscale("log")
+plt.show()
